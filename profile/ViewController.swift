7d254dbb3f46f164d640c8cd1630b9c55200eddf
Hinako Nakai <hinako.0113n@gmail.com>
2022-05-15 02:11:22 +0900

    Initial Commit

---
+++ profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  profile
+//
+//  Created by 中井日向子 on 2022/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
